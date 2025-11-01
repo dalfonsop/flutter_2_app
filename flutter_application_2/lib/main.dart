70fa4c181d2f018670ffa17c5ab3e6f8d100e360
dalfonsop <dalfonsop94@gmail.com>
2025-10-31 23:26:06 -0500

    Add Chat view widget

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/config/themes/app_theme.dart';
+import 'package:flutter_application_2/presentation/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,17 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      title: 'Yes No App',
       theme: AppTheme(selectedColor: 0).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body:  Center(
-          child: FilledButton.tonal(onPressed: (){}, child: Text('Click me')),
-        ),
-      ),
+      home: ChatScreen()
     );
   }
 }
---
+++ flutter_application_2/lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://i.imgur.com/BC5UTzo.png'),
+          ),
+        ),
+        title: Text('Mi moe'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text('Indice $index');
+              },
+            )),
+            Text('Hola mundo'),
+          ],
+        ),
+      ),
+    );
+  }
+}
