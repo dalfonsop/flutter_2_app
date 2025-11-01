3d293b5ff9c0febe20a3d512335a14d1aae31096
dalfonsop <dalfonsop94@gmail.com>
2025-10-31 22:45:11 -0500

    Customized Apptheme

---
+++ flutter_application_2/lib/config/themes/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color.fromARGB(199, 142, 222, 14);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.black,
+  Colors.white54,
+  Colors.cyanAccent,
+  Colors.teal,
+  Colors.purple
+];
+
+class AppTheme{
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert( selectedColor >= 0 && selectedColor <= _colorThemes.length-1,'Colors must be between 0 and ${_colorThemes.length}');
+
+  ThemeData  theme (){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor],
+      brightness: Brightness.dark
+
+    );
+  }
+}
