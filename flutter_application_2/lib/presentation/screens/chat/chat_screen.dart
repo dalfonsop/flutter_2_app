a77db803a784030f5779584ddbd8ba66d2bf1e1d
dalfonsop <dalfonsop94@gmail.com>
2025-11-02 08:48:33 -0500

    Add behaviors to textformfield & buttoms

--- flutter_application_2/lib/presentation/screens/chat/chat_screen.dart
+++ flutter_application_2/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:flutter_application_2/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:flutter_application_2/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -32,11 +33,12 @@ class _ChatView extends StatelessWidget {
         child: Column(
           children: [
             Expanded(child: ListView.builder(
+              itemCount: 100,
               itemBuilder: (context, index) {
                 return  (index % 2 == 0 ) ? HerMessageBubble() : MyMessageBubble();
               },
             )),
-            Text('Hola mundo'),
+            MessageFieldBox(),
           ],
         ),
       ),
